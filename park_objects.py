53b1c5422b03182f6e7063c6d92525ae3e07064e
aquarapax <aquarapax@gmail.com>
2024-12-02 11:06:18 +0400

    Добавлены классы attraction и theater

---
+++ park_objects.py
@@ -0,0 +1,61 @@
+from location_action import Location_Action
+
+class attraction(Location_Action):
+    '''Класс аттракциона с добавлением свойств длительности и уровня фана'''
+
+    def __init__(self, name: str, address: str, opening_hours: tuple = (), 
+                 description: str = '', active: bool = True,
+                 number_of_seats: int = 0, duration: int = 0, fun_level: int = 0):
+        super().__init__(name=name, address = address, opening_hours = opening_hours, description = description, 
+                         active = active, number_of_seats = number_of_seats)
+        self.__duration = duration  # длительность действия в минутах
+        self.__fun_level = fun_level  # уровень фана 
+
+    @property
+    def duration(self):
+        return self.__duration
+
+    @property
+    def fun_level(self):
+        return self.__fun_level
+    
+    def __str__(self):
+        return f"{self.name} (Адрес: {self.address}, Длительность: {self.duration} мин, Уровень фана: {self.fun_level})"
+
+
+if __name__ == "__main__":
+    attraction1 = attraction("Карусель", "У озера", opening_hours = (10, 20), description="Средненькая карусель",
+                                number_of_seats=20, duration=8, fun_level=5
+                                )
+    print(attraction1)
+    print("Общее количество мест:", attraction1.number_of_seats)
+    print("Посадочных мест:", attraction1.free_seats)
+
+    # Продажа билетов
+    attraction1.sell_ticket(5)
+    print("Свободных мест:", attraction1.free_seats)
+    
+
+
+class theater(Location_Action):
+    '''Класс театра с добавлением свойств списка актеров'''
+
+    def __init__(self, name: str, address: str, opening_hours: tuple = (), 
+                 description: str = '', active: bool = True,
+                 number_of_seats: int = 0, actors: tuple = ()):
+        super().__init__(name=name, address = address, opening_hours = opening_hours, description = description, 
+                         active = active, number_of_seats = number_of_seats)
+        self.__actors = actors  # Список актеров
+
+    @property
+    def actors(self):
+        return self.__actors
+    
+    def __str__(self):
+        return f"{self.name}  Список акторов: {self.actors})"
+    
+if __name__ == "__main__":
+    theater1 = theater("Театр Супер", "За забором", opening_hours = (10, 20), description="Самый крутой театр",
+                                number_of_seats=150, actors=('Иванов', 'Петров', 'Сидоров', 'Дж.Клуни')
+                                )
+    print(theater1)
